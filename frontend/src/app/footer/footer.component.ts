acd4bdf07fbef3def1da272e938c65098ffd622c
saloni darji <salonidarji3335@gmail.com>
2019-06-06 18:42:38 +0530

    redesign of home page

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Category } from "../models/category";
+import { CategoryServiceService } from "../services/category-service.service";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  category_arr: Category[];
+  constructor(private _category: CategoryServiceService) {}
 
   ngOnInit() {
+    this._category.getAllCategory().subscribe(
+      (data: any) => {
+        this.category_arr = data;
+        console.log("inside:" + this.category_arr);
+      },
+      function(err) {
+        console.log(err);
+      },
+      function() {
+        console.log("finally category");
+      }
+    );
   }
-
 }
